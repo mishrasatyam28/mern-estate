d155530c20124c544a942708f891cbe4a6e1b71f
Satyam Mishra <satyamsumit8750@gmail.com>
2024-05-10 23:15:56 +0530

    created deleteUser api route

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { helloWorld, updateUser } from "../controllers/user.controller.js";
+import {
+  deleteUser,
+  helloWorld,
+  updateUser,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/hello-world", helloWorld);
 router.post("/update/:id", verifyToken, updateUser);
+router.post("/delete/:id", verifyToken, deleteUser);
 
 export default router;
