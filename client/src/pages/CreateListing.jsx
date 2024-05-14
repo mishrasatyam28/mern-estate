953932d7a23808f2778c8103d10d72ce7a02f4e8
Satyam Mishra <satyamsumit8750@gmail.com>
2024-05-14 22:29:15 +0530

    adding upload listing image functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,62 @@
-import React from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { useState } from "react";
+import { app } from "../firebase";
 
 function CreateListing() {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({ ImageUrls: [] });
+  console.log(formData);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleImageSubmit = (e) => {
+    if (files.length > 0 && files.length < 7) {
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises).then((urls) => {
+        setFormData({
+          ...formData,
+          ImageUrls: formData.ImageUrls.concat(urls),
+        });
+      });
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -9,6 +65,7 @@ function CreateListing() {
       <form className="flex flex-col sm:flex-row gap-4">
         <div className="flex flex-col gap-4 flex-1">
           <input
+            onChange={handleChange}
             type="text"
             placeholder="Name"
             className="border p-3 rounded-lg"
@@ -18,6 +75,7 @@ function CreateListing() {
             required
           />
           <textarea
+            onChange={handleChange}
             type="text"
             placeholder="Description"
             className="border p-3 rounded-lg"
@@ -25,6 +83,7 @@ function CreateListing() {
             required
           />
           <input
+            onChange={handleChange}
             type="text"
             placeholder="Address"
             className="border p-3 rounded-lg"
@@ -33,23 +92,48 @@ function CreateListing() {
           />
           <div className="flex gap-6 flex-wrap">
             <div className=" flex gap-2">
-              <input type="checkbox" id="sale" className="w-5" />
+              <input
+                onChange={handleChange}
+                type="checkbox"
+                id="sale"
+                className="w-5"
+              />
               <span>Sell</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="sale" className="w-5" />
+              <input
+                onChange={handleChange}
+                type="checkbox"
+                id="rent"
+                className="w-5"
+              />
               <span>Rent</span>
             </div>
             <div className=" flex gap-2">
-              <input type="checkbox" id="sale" className="w-5" />
+              <input
+                onChange={handleChange}
+                type="checkbox"
+                id="parking"
+                className="w-5"
+              />
               <span>Parking spot</span>
             </div>
             <div className=" flex gap-2">
-              <input type="checkbox" id="sale" className="w-5" />
+              <input
+                onChange={handleChange}
+                type="checkbox"
+                id="furnished"
+                className="w-5"
+              />
               <span>Furnished</span>
             </div>
             <div className=" flex gap-2">
-              <input type="checkbox" id="sale" className="w-5" />
+              <input
+                onChange={handleChange}
+                type="checkbox"
+                id="offer"
+                className="w-5"
+              />
               <span>Offer</span>
             </div>
           </div>
@@ -57,6 +141,7 @@ function CreateListing() {
           <div className="flex flex-wrap gap-6">
             <div className="flex gap-2 items-center">
               <input
+                onChange={handleChange}
                 className="p-3 border border-gray-300 rounded-lg"
                 type="number"
                 id="bedrooms"
@@ -68,6 +153,7 @@ function CreateListing() {
             </div>
             <div className="flex gap-2 items-center">
               <input
+                onChange={handleChange}
                 className="p-3 border border-gray-300 rounded-lg"
                 type="number"
                 id="bathrooms"
@@ -79,6 +165,7 @@ function CreateListing() {
             </div>
             <div className="flex gap-2 items-center">
               <input
+                onChange={handleChange}
                 className="p-3 border border-gray-300 rounded-lg"
                 type="number"
                 id="regularPrice"
@@ -93,6 +180,7 @@ function CreateListing() {
             </div>
             <div className="flex gap-2 items-center">
               <input
+                onChange={handleChange}
                 className="p-3 border border-gray-300 rounded-lg"
                 type="number"
                 id="discountPrice"
@@ -119,13 +207,18 @@ function CreateListing() {
           </p>
           <div className="flex gap-4">
             <input
+              onChange={(e) => setFiles(e.target.files)}
               className="p-3 border border-gray-300 rounded w-full"
               type="file"
               id="images"
               accept="image/*"
               multiple
             />
-            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+            <button
+              type="button"
+              onClick={handleImageSubmit}
+              className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+            >
               Upload
             </button>
           </div>
