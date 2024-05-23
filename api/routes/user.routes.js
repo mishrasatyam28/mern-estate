00132961debc11ae9ccb9d72caeb0c8cafade78f
Satyam Mishra <satyamsumit8750@gmail.com>
2024-05-23 09:01:27 +0530

    create get user listing api route

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -3,6 +3,7 @@ import {
   deleteUser,
   helloWorld,
   updateUser,
+  getUserListings,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/hello-world", helloWorld);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
