91c0fc2ed18b1856ea43e97900f756ea54670a1a
Satyam Mishra <satyamsumit8750@gmail.com>
2024-05-24 21:01:25 +0530

    bug fixing...

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -50,9 +50,9 @@ export const deleteUser = async (req, res, next) => {
 };
 
 export const getUserListings = async (req, res, next) => {
-  if (req.user._id == req.params.id) {
+  if (req.user.id === req.params.id) {
     try {
-      const listings = await Listing.find({ userRef: req.params._id });
+      const listings = await Listing.find({ userRef: req.params.id });
       res.status(200).json(listings);
     } catch (error) {
       next(error);
