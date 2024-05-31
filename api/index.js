69c3e3c0e4ccb6fcce686b49052f40a421a700df
Satyam Mishra <satyamsumit8750@gmail.com>
2024-05-31 20:48:25 +0530

    remove import things (json)

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express  from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
