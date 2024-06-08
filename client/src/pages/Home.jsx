66dca97651f4ba6fd9c49eb4d6015316e3bbea94
Satyam Mishra <satyamsumit8750@gmail.com>
2024-06-08 23:39:57 +0530

    building home page...

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <div>Home</div>;
+  const [offerListing, setOfferListing] = useState([]);
+  const [saleListing, setSaleListing] = useState([]);
+  const [rentListing, setRentListing] = useState([]);
+
+  useEffect(() => {
+    const fetchOfferListing = async () => {
+      try {
+        const res = await fetch("/api/listing/get?offer=true&limit=4");
+        const data = await res.json();
+
+        setOfferListing(data);
+        fetchRentLisitngs();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const fetchRentLisitngs = async () => {
+      try {
+        const res = await fetch("/api/listing/get?type=rent&limit=4");
+        const data = await res.json();
+
+        setRentListing(data);
+        fetchSaleLisitngs();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const fetchSaleLisitngs = async () => {
+      try {
+        const res = await fetch("/api/listing/get?type=sale&limit=4");
+        const data = await res.json();
+
+        setSaleListing(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchOfferListing();
+  }, []);
+
+  return (
+    <div>
+      {/* top */}
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+          Find your next <span className="text-slate-500">perfect</span>
+          <br />
+          place with ease
+        </h1>
+        <div className="text-gray-400 text-xs sm:text-sm">
+          Satyam Estate is the best place to find your next perfect place to
+          live.
+          <br />
+          We have wide range of you to choose from.
+        </div>
+        <Link
+          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
+          to={"/search"}
+        >
+          Let's get started...
+        </Link>
+      </div>
+      {/* swiper */}
+
+      {/* projects */}
+    </div>
+  );
 }
 
 export default Home;
